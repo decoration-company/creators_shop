1d57669fc607e13f131dbb7ddf944d11aa9a43ab
Kazuya Tamakoshi <kazuya.tamakoshi@franliber.co.jp>
2016-07-14 01:12:10 +0900

    Upgrade from Rails4.2 to Rails5.0

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -19,9 +18,8 @@ ActiveRecord::Schema.define(version: 20160712073624) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
-
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -30,9 +28,8 @@ ActiveRecord::Schema.define(version: 20160712073624) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end

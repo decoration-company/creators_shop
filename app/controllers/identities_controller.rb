39939a9c81512fd33f1225ae3af34ed80e5f274d
Rie Narita <rie.narita+git@franliber.co.jp>
2016-07-18 14:13:22 +0900

    Integrate flash messages

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -16,27 +16,30 @@ class IdentitiesController < ApplicationController
         # account. But we found the identity and the user associated with it
         # is the current user. So the identity is already associated with
         # this user. So let's display an error message.
-        redirect_to root_url, notice: t('msg.already_linked')
+        flash[:danger] = t('msg.already_linked')
+        redirect_to root_url
       else
         # The identity is not associated with the current_user so lets
         # associate the identity
         @identity.user = current_user
         @identity.save()
-        redirect_to root_url, notice: t('msg.linked_successfully')
+        flash[:success] = t('msg.linked_successfully')
+        redirect_to root_url
       end
     else
       if @identity.user.present?
         # The identity we found had a user associated with it so let's
         # just log them in here
         sign_in @identity.user
-        redirect_to root_url, notice: t('msg.signed_in')
+        flash[:info] = t('msg.signed_in')
+        redirect_to root_url
       else
         # No user associated with the identity so we need to create a new one
         user = User.create_with_omniauth(auth)
         user.identities << @identity
         sign_in user
-
-        redirect_to root_url, notice: t('msg.finish_registerging')
+        flash[:info] = t('msg.finished_registering')
+        redirect_to root_url
       end
     end
   end
@@ -44,7 +47,8 @@ class IdentitiesController < ApplicationController
   def destroy
     provider = params[:provider]
     if current_user.identities.count == 1
-      redirect_to :back, notice: t('msg.cannot_remove_all_accounts')
+      flash[:danger] = t('msg.cannot_remove_all_accounts')
+      redirect_to :back
     else
       identity = Identity.find_by_provider_and_user_id(provider, current_user.id)
       identity.destroy

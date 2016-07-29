9eb617f9dfa591176f63db6ea0b812f52312a623
toshiwo <toshiwo@toshiwo.com>
2016-07-29 15:58:04 +0900

    fix database_rewinder configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -57,4 +57,8 @@ RSpec.configure do |config|
 
   config.include Rails.application.routes.url_helpers
   config.include Capybara::DSL
+
+  config.before(:suite) { DatabaseRewinder.clean_all }
+
+  config.after(:each) { DatabaseRewinder.clean }
 end

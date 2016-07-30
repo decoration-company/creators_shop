ff65d593d2eb70bf6844cd7b8740a4c1b692502e
toshiwo <toshiwo@toshiwo.com>
2016-07-31 05:30:43 +0900

    refactoring a class methods definication of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,35 +14,39 @@ class User < ActiveRecord::Base
   before_create :create_remember_token
   before_save { self.email = email.downcase }
 
-  def self.create_with_omniauth(auth)
-    auth['info']['email'] ||= User.dummy_email(auth)
-    create(name: auth['info']['name'], email: auth['info']['email'], image_url: auth['info']['image'], password: User.friendly_token(20))
+  class << self
+    def create_with_omniauth(auth)
+      auth['info']['email'] ||= dummy_email(auth)
+      create(name: auth['info']['name'], email: auth['info']['email'], image_url: auth['info']['image'], password: friendly_token(20))
 
-    # Facebook：activated:true
-    # Twitter：activated:false
-  end
+      # Facebook：activated:true
+      # Twitter：activated:false
+    end
 
-  def User.new_remember_token
-    SecureRandom.urlsafe_base64
-  end
+    def new_remember_token
+      SecureRandom.urlsafe_base64
+    end
 
-  def User.encrypt(token)
-    Digest::SHA1.hexdigest(token.to_s)
+    def encrypt(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    def dummy_email(auth)
+      "#{auth.uid}-#{auth.provider}@example.com"
+    end
+    private :dummy_email
+
+    def friendly_token(length = 20)
+      rlength = (length * 3) / 4
+      SecureRandom.urlsafe_base64(rlength).tr('lIO0', 'sxyz')
+    end
+    private :friendly_token
   end
 
   private
 
     def create_remember_token
       self.remember_token ||= User.encrypt(User.new_remember_token)
-    end
-
-    def self.dummy_email(auth)
-      "#{auth.uid}-#{auth.provider}@example.com"
-    end
-
-    def self.friendly_token(length = 20)
-      rlength = (length * 3) / 4
-      SecureRandom.urlsafe_base64(rlength).tr('lIO0', 'sxyz')
     end
 
 end
